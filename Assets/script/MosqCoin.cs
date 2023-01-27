e40cd909ce9af1e534dcbdf0b8c5452244f9baa5
KyeongMinKim <7794rudals@naver.com>
2023-01-27 16:52:27 +0900

    Hp
    update

--- Assets/script/MosqCoin.cs
+++ Assets/script/MosqCoin.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 
 public class MosqCoin : MonoBehaviour
 {
-  public static float MosqCoins;
+  public static int MosqCoins;
 
   void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag.Equals("Player"))
         {
             Destroy(this.gameObject);
-            MosqCoins += 1.0f;
+            
+            MosqCoins += 1;
         }
-    }
-
-      void OnBecameInvisible() //카메라 시야 밖으로 벗어나면> 조건 함수
-    {
-        Destroy(gameObject); //파괴
     }
 }
